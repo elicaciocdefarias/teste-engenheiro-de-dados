24d88db5722df2a9eb6cace84dc0b51cffdc4c4d
Elicacio C. de Farias <elicaciocardoso@gmail.com>
2022-07-21 14:59:09 -0300

    corrigi as chaves primaria e estrangeira das tabelas

--- work-dir/sql/cria_tabelas.sql
+++ work-dir/sql/cria_tabelas.sql
@@ -1,23 +1,21 @@
 use microdados;
 
 CREATE TABLE IF NOT EXISTS dimensao_escola (
-    ID int NOT NULL AUTO_INCREMENT,
-    ID_ESCOLA int,
+    ID_ESCOLA bigint NOT NULL AUTO_INCREMENT,
     CODIGO_MUNICIPIO int,
     NOME_MUNICIPIO varchar(255),  
     DEPENDENCIA varchar(255),
     LOCALIZACAO varchar(255),
     SITUACAO varchar(255),
-    PRIMARY KEY (ID)
+    PRIMARY KEY (ID_ESCOLA)
 );
 
 CREATE TABLE IF NOT EXISTS dimensao_aluno (
-    ID int NOT NULL AUTO_INCREMENT,
-    ID_ALUNO int,
+    ID_ALUNO bigint NOT NULL AUTO_INCREMENT,
     NU_INSCRICAO int, 
     ETNIA varchar(255),
     SEXO varchar(255),
-    PRIMARY KEY (ID)
+    PRIMARY KEY (ID_ALUNO)
 );
 
 CREATE TABLE IF NOT EXISTS dimensao_tempo (
@@ -27,16 +25,15 @@ CREATE TABLE IF NOT EXISTS dimensao_tempo (
 );
 
 CREATE TABLE IF NOT EXISTS fato_avaliacao (
-    ID int NOT NULL AUTO_INCREMENT,
-    ID_AVALIACAO int,
+    ID_AVALIACAO bigint NOT NULL AUTO_INCREMENT,
     TIPO varchar(255),
     NOTA double(255, 1),
     PRESENCA varchar(255),
-    ID_ESCOLA int,
-    ID_ALUNO int,
+    ID_ESCOLA bigint,
+    ID_ALUNO bigint,
     ID_TEMPO int,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (ID_ESCOLA) REFERENCES dimensao_escola(ID),
-    FOREIGN KEY (ID_ALUNO) REFERENCES dimensao_aluno(ID),
+    PRIMARY KEY (ID_AVALIACAO),
+    FOREIGN KEY (ID_ESCOLA) REFERENCES dimensao_escola(ID_ESCOLA),
+    FOREIGN KEY (ID_ALUNO) REFERENCES dimensao_aluno(ID_ALUNO),
     FOREIGN KEY (ID_TEMPO) REFERENCES dimensao_tempo(ID)
 );
